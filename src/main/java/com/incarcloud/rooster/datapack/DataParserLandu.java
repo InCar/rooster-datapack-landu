04fbfea9f028fc1aba7e36abee11691719257934
jack-luj <wjsc@qq.com>
2017-06-01 14:50:02 +0800

    parser

--- src/main/java/com/incarcloud/rooster/datapack/DataParserLandu.java
+++ src/main/java/com/incarcloud/rooster/datapack/DataParserLandu.java
@@ -1,16 +1,108 @@
 package com.incarcloud.rooster.datapack;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.buffer.Unpooled;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class DataParserLANDU implements IDataParser {
+/**
+ * LANDU Parser.
+ *
+ * @author Aaric
+ * @since 2.0
+ */
+public class DataParserLandu implements IDataParser {
+
+    /**
+     * 反转byte数组
+     *
+     * @param bytes byte数组
+     * @return byte数组
+     */
+    private static byte[] reverseByteArray(final byte[] bytes) {
+        if(null == bytes) {
+            throw new IllegalArgumentException("bytes is null.");
+        }
+        byte[] newBytes = new byte[bytes.length];
+        for(int i = 0; i < bytes.length; i++) {
+            newBytes[i] = (byte) ~bytes[i];
+        }
+        return newBytes;
+    }
+
+    /**
+     * 2字节的byte数组转整型
+     *
+     * @param bytes byte数组
+     * @return 整型
+     */
+    private static int byteArray2ToInt(final byte[] bytes) {
+        byte[] newBytes = new byte[]{0x00, 0x00, 0x00, 0x00};
+        newBytes[2] = bytes[0];
+        newBytes[3] = bytes[1];
+        ByteBuf buffer = Unpooled.wrappedBuffer(newBytes);
+        int integer = buffer.getInt(0);
+        buffer.release();
+        return  integer;
+    }
 
     @Override
     public List<DataPack> extract(ByteBuf buffer){
-        List<DataPack> listPacks = new ArrayList<>();
+        /**
+         * ## LANDU数据包格式 ##
+         * 0,1: 数据包标志(AA55)
+         * 2,3: 数据包长度
+         * 4,5: 数据包长度校验(数据包长度取反)
+         * 6: 数据包ID
+         * 7: 保留字节(协议格式版本, v2.05-0x02, v3.08-0x05)
+         * ...: 数据内容(其长度为【数据包长度】– 4 – 2)
+         * 最后2字节: 校验和(【数据包长度】开始至全部【数据内容】结束止的所有字节产累加之和)
+         */
+        List<DataPack> dataPackList = new ArrayList<>();
+        int total = buffer.readableBytes();
+        // 1.数据包长度必须大于9个字节(最小10个字节)
+        if(9 < total) {
+            // 2.判断数据包标志
+            byte[] flagBytes = ByteBufUtil.getBytes(buffer, 0, 2);
+            String flagBytesString = ByteBufUtil.hexDump(flagBytes).toUpperCase();
+            if("AA55".equals(flagBytesString)) {
+                // 3.校验数据包长度(取反校验+数值校验)
+                if(buffer.getByte(2) == ~buffer.getByte(4) && buffer.getByte(3) == ~buffer.getByte(5)) {
+                    int length = byteArray2ToInt(ByteBufUtil.getBytes(buffer, 2, 2));
+                    // 长度 = 总长度 - 2(2字节标志位)
+                    if(length == (total - 2)) {
+                        // 4.校验和校验
+                        /*int sum = 0;
+                        for (int i = 2; i < total - 2; i++) {
+                            System.out.println(DatatypeConverter.printHexBinary(new byte[]{buffer.getByte(i)}));
+                            sum += buffer.getByte(i);
+                        }
+                        System.out.println(sum);
+                        byte[] bs = new byte[]{buffer.getByte(total-2), buffer.getByte(total-1)};
+                        System.out.println(DatatypeConverter.printHexBinary(ByteBufUtil.getBytes(buffer, total-2, 2)));
+                        System.out.println(byteArray2ToInt(ByteBufUtil.getBytes(buffer, total-2, 2)));*/
+                        // 5.打包(第7个字节为协议格式版本)
+                        String version = "";
+                        switch (buffer.getByte(7)) {
+                            case 0x02:
+                                version = "2.05";
+                                break;
+                            case 0x05:
+                                version = "3.08";
+                                break;
+                            default:
+                                version = "unknown";
+                        }
+                        DataPack dataPack = new DataPack("china", "landu", version);
+                        dataPack.setBuf(buffer.slice(0, total));
+                        dataPackList.add(dataPack);
+                    }
+                }
+            }
+        }
 
-        return listPacks;
+        return dataPackList;
     }
 }
--- src/test/java/com/incarcloud/rooster/datapack/DataParserLanduTest.java
+++ src/test/java/com/incarcloud/rooster/datapack/DataParserLanduTest.java
@@ -1,6 +1,7 @@
 package com.incarcloud.rooster.datapack;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
 import io.netty.buffer.Unpooled;
 import org.junit.Assert;
 import org.junit.Test;
@@ -9,6 +10,60 @@ import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
-public class DataParserLANDUTest {
-    private static Logger s_logger = LoggerFactory.getLogger(DataParserLANDUTest.class);
+/**
+ * Test DataParserLANDU class
+ *
+ * @author Aaric
+ * @since 2.0
+ */
+public class DataParserLanduTest {
+
+    private static Logger s_logger = LoggerFactory.getLogger(DataParserLanduTest.class);
+
+    @Test
+    public void testReverseByteArray() throws Exception {
+        byte tmp;
+        byte[] bytes = {(byte) 0xFF,(byte) 0xB4};
+        for(int i = 0; i < bytes.length; i++) {
+            tmp = bytes[i];
+            bytes[i] = (byte) ~tmp;
+        }
+        /*byte[] bytes = {(byte) 0xFF,(byte) 0xB4};
+        bytes = DataParserLANDU.reverseByteArray(bytes);*/
+        Assert.assertEquals("004b", ByteBufUtil.hexDump(bytes));
+    }
+
+    @Test
+    public void testByteArray2ToInt() throws Exception {
+        byte[] bytes = {0x00, 0x00, 0x00, 0x4B};
+        ByteBuf buffer = Unpooled.wrappedBuffer(bytes);
+        Assert.assertEquals(75, buffer.getInt(0));
+        buffer.release();
+        /*byte[] bytes = {0x00, 0x4B};
+        Assert.assertEquals(75, DataParserLANDU.byteArray2ToInt(bytes));*/
+    }
+
+    @Test
+    public void testExtract() throws Exception {
+        // 测试数据-从服务器取得参数
+        byte[] data = {
+                (byte) 0xAA, 0x55, //数据包标志(AA55)-2字节
+                0x00, 0x4B, //数据包长度-2字节
+                (byte) 0xFF,(byte) 0xB4, //数据包长度校验(数据包长度取反)-2字节
+                0x00, //数据包ID-1字节
+                0x05, //保留字节(协议格式版本, v2.05-0x02, v3.08-0x05)-1字节
+                0x16, 0x03, 0x49, 0x4E, 0x43, 0x41, 0x52, 0x30, 0x30, 0x30, 0x30, 0x30, 0x31, 0x00, 0x00, 0x00, 0x00, (byte) 0xCC, 0x35, 0x00, 0x4C, 0x53, 0x47, 0x50, 0x42, 0x36, 0x34, 0x55, 0x35, 0x42, 0x53, 0x32, 0x34, 0x30, 0x34, 0x36, 0x39, 0x00, 0x56, 0x31, 0x2E, 0x35, 0x30, 0x2E, 0x30, 0x30, 0x00, 0x56, 0x30, 0x2E, 0x30, 0x30, 0x2E, 0x30, 0x30, 0x00, 0x56, 0x33, 0x2E, 0x31, 0x34, 0x2E, 0x30, 0x32, 0x00, (byte) 0xFF, 0x00,
+                0x0F, (byte) 0xDA //校验和(【数据包长度】开始至全部【数据内容】结束止的所有字节产累加之和)-2字节
+        };
+        ByteBuf buffer = Unpooled.wrappedBuffer(data);
+        IDataParser parser = new DataParserLandu();
+        List<DataPack> dataPackList = parser.extract(buffer);
+        Assert.assertEquals(1, dataPackList.size());
+
+        for(DataPack dataPack: dataPackList) {
+            dataPack.freeBuf();
+        }
+        buffer.release();
+        Assert.assertEquals(0, buffer.refCnt());
+    }
 }

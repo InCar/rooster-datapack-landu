54fae68270e45e9e53347a19abe209cb7f275255
youbeiwuhuan <fan794919764@126.com>
2017-08-30 17:04:06 +0800

    单词错误

--- src/main/java/com/incarcloud/rooster/datapack/LanduCommandFacotry.java
+++ src/main/java/com/incarcloud/rooster/datapack/LanduCommandFacotry.java
@@ -10,7 +10,7 @@ import io.netty.buffer.ByteBuf;
  * @Description: 描述
  * @date 2017/7/21 14:38
  */
-public class LanduCommandFacotry implements CommandFacotry {
+public class LanduCommandFacotry implements CommandFactory {
 
     static {
         CommandFacotryManager.registerCommandFacotry(DataParserLandu.PROTOCOL_PREFIX + "2.05", LanduCommandFacotry.class);
@@ -24,7 +24,7 @@ public class LanduCommandFacotry implements CommandFacotry {
      * @param type 命令类型
      * @return
      */
-    public ByteBuf createCommand(CommandType type) {
+    public ByteBuf createCommand(CommandType type, Object... args) {
         // 目前LANDU(2.05/3.08)不支持CommandType类型
         return null;
     }

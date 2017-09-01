4ea2e8ffb4b6220bfa41be718171d3c1a19380cf
youbeiwuhuan <fan794919764@126.com>
2017-09-01 10:30:12 +0800

    移动类

--- src/main/java/com/incarcloud/rooster/datapack/LanduCommandFacotry.java
+++ src/main/java/com/incarcloud/rooster/datapack/LanduCommandFacotry.java
@@ -2,6 +2,8 @@ package com.incarcloud.rooster.datapack;/**
  * Created by fanbeibei on 2017/7/21.
  */
 
+import com.incarcloud.rooster.gather.cmd.CommandFacotryManager;
+import com.incarcloud.rooster.gather.cmd.CommandFactory;
 import com.incarcloud.rooster.gather.cmd.CommandType;
 import io.netty.buffer.ByteBuf;
 
